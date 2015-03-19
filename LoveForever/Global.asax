fe0eae7520cd11ad72c3b905c44236ef7bd4b2c1
YingXia <191502003@qq.com>
2015-03-19 20:15:03 +0800

    project

---
+++ LoveForever/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoveForever.MvcApplication" Language="C#" %>
---
+++ LoveForever/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoveForever.Startup))]
+namespace LoveForever
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
